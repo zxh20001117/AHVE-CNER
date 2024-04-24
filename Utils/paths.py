f1294f4b8dc7b4a85830d51aaa647216896d372b
F4riK1ller <287961528@qq.com>
2024-04-24 22:21:27 +0800

    initial commit

--- Utils/paths.py
+++ Utils/paths.py
@@ -1,9 +1,7 @@
-root_path = "D:/多模态任务/MMW2NER/"
-# root_path = "/root/autodl-tmp/MMW2NER/"
+# root_path = "D:/多模态任务/AHVE-CNER/"
+root_path = "/root/autodl-tmp/AHVE-CNER/"
 pinyin_vocab_path = "data/pingyin_vocab.npy"
 radical_vocab_path = "data/chaizi-jt.txt"
-# bert_path = "model/bert-base-chinese"
-# bert_path = "model/roberta-wwm-large"
 bert_path = "model/bert-wwm-chinese"
 bert_path = 'model/guwenbert-large'
 ttf_paths = ['data/ttf/simhei.ttf', 'data/ttf/fangsong.ttf', 'data/ttf/kaiti.ttf']
